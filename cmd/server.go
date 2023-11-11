92fb401e50b2981fe427a381bc97f606becffc92
Alejandro Falkowski <alexrfalkowski@gmail.com>
2023-11-11 14:00:38 +0100

    feat(deps): update github.com/alexfalkowski/go-service to v1.110.0

--- cmd/server.go
+++ cmd/server.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"github.com/alexfalkowski/go-service-template/config"
 	"github.com/alexfalkowski/go-service-template/server/health"
+	"github.com/alexfalkowski/go-service/debug"
 	"github.com/alexfalkowski/go-service/runtime"
 	"github.com/alexfalkowski/go-service/telemetry"
 	"github.com/alexfalkowski/go-service/telemetry/metrics"
@@ -12,7 +13,7 @@ import (
 
 // ServerOptions for cmd.
 var ServerOptions = []fx.Option{
-	fx.NopLogger, runtime.Module,
+	fx.NopLogger, runtime.Module, debug.Module,
 	telemetry.Module, metrics.Module, Module,
 	config.Module, health.Module, transport.Module,
 }
