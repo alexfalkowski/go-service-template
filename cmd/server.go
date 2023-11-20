724f2eaa4f925a70c494ad0a7ce1cb5769eb765e
Alejandro Falkowski <alexrfalkowski@gmail.com>
2023-11-20 07:36:26 +0100

    feat(deps): update github.com/alexfalkowski/go-service to v1.119.0 (#307)

--- cmd/server.go
+++ cmd/server.go
@@ -3,11 +3,11 @@ package cmd
 import (
 	"github.com/alexfalkowski/go-service-template/config"
 	"github.com/alexfalkowski/go-service-template/server/health"
+	"github.com/alexfalkowski/go-service-template/transport"
 	"github.com/alexfalkowski/go-service/debug"
 	"github.com/alexfalkowski/go-service/runtime"
 	"github.com/alexfalkowski/go-service/telemetry"
 	"github.com/alexfalkowski/go-service/telemetry/metrics"
-	"github.com/alexfalkowski/go-service/transport"
 	"go.uber.org/fx"
 )
 
