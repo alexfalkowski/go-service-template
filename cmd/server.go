9dfeb39e86aef759621a0715276959840fa04087
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-05-04 21:18:01 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.178.0 (#503)

--- cmd/server.go
+++ cmd/server.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"github.com/alexfalkowski/go-service-template/config"
 	"github.com/alexfalkowski/go-service-template/server/health"
-	"github.com/alexfalkowski/go-service-template/transport"
 	"github.com/alexfalkowski/go-service/compressor"
 	"github.com/alexfalkowski/go-service/debug"
 	"github.com/alexfalkowski/go-service/feature"
@@ -11,6 +10,7 @@ import (
 	"github.com/alexfalkowski/go-service/runtime"
 	"github.com/alexfalkowski/go-service/telemetry"
 	"github.com/alexfalkowski/go-service/telemetry/metrics"
+	"github.com/alexfalkowski/go-service/transport"
 	"go.uber.org/fx"
 )
 
--- config/config.go
+++ config/config.go
@@ -2,11 +2,27 @@ package config
 
 import (
 	"github.com/alexfalkowski/go-service-template/health"
+	"github.com/alexfalkowski/go-service/cmd"
 	"github.com/alexfalkowski/go-service/config"
 )
+
+// NewConfig for config.
+func NewConfig(i *cmd.InputConfig) (*Config, error) {
+	c := &Config{}
+
+	return c, i.Unmarshal(c)
+}
 
 // Config for the service.
 type Config struct {
 	Health         *health.Config `yaml:"health,omitempty" json:"health,omitempty" toml:"health,omitempty"`
 	*config.Config `yaml:",inline" json:",inline" toml:",inline"`
+}
+
+func decorateConfig(cfg *Config) *config.Config {
+	return cfg.Config
+}
+
+func healthConfig(cfg *Config) *health.Config {
+	return cfg.Health
 }
