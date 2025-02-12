e249730e9f16c4d368d58a7f93111d2998ded17c
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-02-12 17:46:34 +0100

    feat(cmd): move to use register (#886)

--- internal/cmd/server.go
+++ internal/cmd/server.go
@@ -3,17 +3,19 @@ package cmd
 import (
 	"github.com/alexfalkowski/go-service-template/internal/config"
 	"github.com/alexfalkowski/go-service-template/internal/health"
+	"github.com/alexfalkowski/go-service/cmd"
 	"github.com/alexfalkowski/go-service/debug"
 	"github.com/alexfalkowski/go-service/feature"
 	"github.com/alexfalkowski/go-service/module"
 	"github.com/alexfalkowski/go-service/telemetry"
 	"github.com/alexfalkowski/go-service/transport"
-	"go.uber.org/fx"
 )
 
-// ServerOptions for cmd.
-var ServerOptions = []fx.Option{
-	module.Module, debug.Module, feature.Module,
-	telemetry.Module, transport.Module,
-	config.Module, health.Module, Module,
+// RegisterServer for cmd.
+func RegisterServer(command *cmd.Command) {
+	command.AddServer("server", "Start go-service-template server",
+		module.Module, debug.Module, feature.Module,
+		telemetry.Module, transport.Module,
+		config.Module, health.Module, Module,
+	)
 }
