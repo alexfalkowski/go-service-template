71f2a2b2aa6523f8d2c65ff4531f4b3a2cec8262
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-05-19 13:48:39 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.614.0 (#1109)
    https://github.com/alexfalkowski/go-service/pull/1608

--- main.go
+++ main.go
@@ -5,19 +5,12 @@ import (
 
 	"github.com/alexfalkowski/go-service-template/internal/cmd"
 	sc "github.com/alexfalkowski/go-service/cmd"
-	"github.com/alexfalkowski/go-service/env"
-	"github.com/alexfalkowski/go-service/os"
 )
 
-func main() {
-	command().ExitOnError(context.Background())
-}
-
-func command() *sc.Command {
-	fs := os.NewFS()
-	command := sc.New(env.NewName(fs), env.NewVersion())
-
+var app = sc.NewApplication(func(command *sc.Command) {
 	cmd.RegisterServer(command)
+})
 
-	return command
+func main() {
+	app.ExitOnError(context.Background())
 }
