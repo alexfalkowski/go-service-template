01ef94d8d0d665e53d244614da2bd140a9ef2f4d
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-06-10 20:37:22 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service/v2 to v2.65.0 (#1184)
    https://github.com/alexfalkowski/go-service/pull/1716

--- internal/cmd/module.go
+++ internal/cmd/module.go
@@ -3,12 +3,12 @@ package cmd
 import (
 	"github.com/alexfalkowski/go-service-template/internal/config"
 	"github.com/alexfalkowski/go-service-template/internal/health"
+	"github.com/alexfalkowski/go-service/v2/di"
 	"github.com/alexfalkowski/go-service/v2/module"
-	"go.uber.org/fx"
 )
 
 // Module for fx.
-var Module = fx.Options(
+var Module = di.Module(
 	module.Server,
 	config.Module,
 	health.Module,
