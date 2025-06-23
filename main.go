d6e3a7a7c0f406f8a3446e1ec858d077a2842d57
Alex Falkowski <alexrfalkowski@gmail.com>
2025-06-23 06:49:55 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service/v2 to v2.78.0
    https://github.com/alexfalkowski/go-service/pull/1737

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"context"
-
 	"github.com/alexfalkowski/go-service-template/internal/cmd"
 	"github.com/alexfalkowski/go-service/v2/cli"
+	"github.com/alexfalkowski/go-service/v2/context"
 )
 
 var app = cli.NewApplication(func(command cli.Commander) {
