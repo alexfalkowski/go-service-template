6139d79ffb031874d9982a9fc755173807343a0c
Alex Falkowski <alexrfalkowski@gmail.com>
2025-07-26 22:02:30 +0200

    feat(health): use just noop for grpc

--- internal/health/health.go
+++ internal/health/health.go
@@ -17,7 +17,7 @@ func register(name env.Name, srv *server.Server, cfg *Config) {
 	}
 
 	srv.Register(name.String(), regs...)
-	srv.Register(v1.Service_ServiceDesc.ServiceName, regs...)
+	srv.Register(v1.Service_ServiceDesc.ServiceName, regs[0])
 }
 
 func httpHealthObserver(name env.Name, server *server.Server) error {
